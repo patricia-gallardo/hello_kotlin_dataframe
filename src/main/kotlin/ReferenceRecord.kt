7953da65d2883cafc84b68de5b5f1476a8049816
patricia-gallardo <psmaas@gmail.com>
2024-11-19 02:33:29 +0100

    Move jackson stuff out of the data class

--- src/main/kotlin/ReferenceRecord.kt
+++ src/main/kotlin/ReferenceRecord.kt
@@ -1,50 +1,9 @@
 package org.example
 
-import com.fasterxml.jackson.dataformat.csv.CsvMapper
-import com.fasterxml.jackson.dataformat.csv.CsvParser
-import com.fasterxml.jackson.dataformat.csv.CsvSchema
-import java.io.BufferedWriter
-import java.io.InputStream
-
 
 data class ReferenceRecord(
     val id: String? = null,
     val list1: List<String>? = null,
     val list2: List<String>? = null,
     val list3: List<String>? = null
-) {
-    companion object {
-
-        fun readCSV(csvMapper: CsvMapper, schema: CsvSchema, inputStream: InputStream): List<ReferenceRecord> {
-            val readList = csvMapper.readerFor(ReferenceRecord::class.java)
-                .with(schema.withSkipFirstDataRow(true))
-                .readValues<ReferenceRecord>(inputStream)
-                .readAll()
-            return readList
-        }
-
-        fun writeCSV(list: List<ReferenceRecord>, writer: BufferedWriter, csvMapper: CsvMapper, schema: CsvSchema) {
-            csvMapper.writer()
-                .with(schema.withHeader())
-                .writeValues(writer)
-                .writeAll(list)
-        }
-
-        fun mapper(): CsvMapper {
-            val csvMapper = CsvMapper()
-                .apply {
-                    enable(CsvParser.Feature.TRIM_SPACES)
-                    enable(CsvParser.Feature.SKIP_EMPTY_LINES)
-                }
-            return csvMapper
-        }
-
-        fun schema(): CsvSchema = CsvSchema.builder()
-            .addColumn("id")
-            .addColumn("list1")
-            .addColumn("list2")
-            .addColumn("list3")
-            .setNullValue("")
-            .build()
-    }
-}
+)
--- src/main/kotlin/ReferenceRecordJacksonIO.kt
+++ src/main/kotlin/ReferenceRecordJacksonIO.kt
@@ -1,7 +1,12 @@
 package org.example
 
+import com.fasterxml.jackson.dataformat.csv.CsvMapper
+import com.fasterxml.jackson.dataformat.csv.CsvParser
+import com.fasterxml.jackson.dataformat.csv.CsvSchema
+import java.io.BufferedWriter
 import java.io.File
 import java.io.FileInputStream
+import java.io.InputStream
 import java.nio.charset.StandardCharsets
 
 class ReferenceRecordJacksonIO {
@@ -10,10 +15,10 @@ class ReferenceRecordJacksonIO {
             val parentFolder = CpvFolder.get(fileName, folder)
             FileInputStream(File(parentFolder, fileName))
                 .use { stream ->
-                    val csvMapper = ReferenceRecord.mapper()
-                    val schema = ReferenceRecord.schema()
+                    val csvMapper = mapper()
+                    val schema = schema()
                     Log.read(parentFolder, fileName)
-                    return ReferenceRecord.readCSV(csvMapper, schema, stream)
+                    return readCSV(csvMapper, schema, stream)
                 }
         }
 
@@ -21,11 +26,43 @@ class ReferenceRecordJacksonIO {
             val parentFolder = CpvFolder.get(fileName, folder)
             File(parentFolder, fileName).bufferedWriter(StandardCharsets.UTF_8)
                 .use { writer ->
-                    val csvMapper = ReferenceRecord.mapper()
-                    val schema = ReferenceRecord.schema()
-                    ReferenceRecord.writeCSV(records, writer, csvMapper, schema)
+                    val csvMapper = mapper()
+                    val schema = schema()
+                    writeCSV(records, writer, csvMapper, schema)
                     Log.write(parentFolder, fileName)
                 }
         }
+
+        private fun readCSV(csvMapper: CsvMapper, schema: CsvSchema, inputStream: InputStream): List<ReferenceRecord> {
+            val readList = csvMapper.readerFor(ReferenceRecord::class.java)
+                .with(schema.withSkipFirstDataRow(true))
+                .readValues<ReferenceRecord>(inputStream)
+                .readAll()
+            return readList
+        }
+
+        private fun writeCSV(list: List<ReferenceRecord>, writer: BufferedWriter, csvMapper: CsvMapper, schema: CsvSchema) {
+            csvMapper.writer()
+                .with(schema.withHeader())
+                .writeValues(writer)
+                .writeAll(list)
+        }
+
+        private fun mapper(): CsvMapper {
+            val csvMapper = CsvMapper()
+                .apply {
+                    enable(CsvParser.Feature.TRIM_SPACES)
+                    enable(CsvParser.Feature.SKIP_EMPTY_LINES)
+                }
+            return csvMapper
+        }
+
+        private fun schema(): CsvSchema = CsvSchema.builder()
+            .addColumn("id")
+            .addColumn("list1")
+            .addColumn("list2")
+            .addColumn("list3")
+            .setNullValue("")
+            .build()
     }
 }
