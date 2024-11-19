5d41789f74f03a5c589d832decfb35293646418d
patricia-gallardo <psmaas@gmail.com>
2024-11-19 20:14:17 +0100

    Extract DataFrames from Jackson parsed csv files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,11 +6,14 @@ private class Config {
     companion object {
         const val TARGET_FOLDER = "src/test/resources/"
         const val DATA_FRAME_FILE_NAME = "dataframe.csv"
+        const val JACKSON_FILE_NAME = "jackson.csv"
     }
 }
 
 // based on https://github.com/Kotlin/dataframe/discussions/717
 fun main() {
-    val df = ReferenceRecordDataframeIO.readDataFrame(Config.DATA_FRAME_FILE_NAME, Config.TARGET_FOLDER)
-    println(df.generateCode(markerName = "ReferenceRecord"))
+    val dataFrame = ReferenceRecordDataframeIO.readDataFrame(Config.DATA_FRAME_FILE_NAME, Config.TARGET_FOLDER)
+    println(dataFrame.generateCode(markerName = "ReferenceRecord"))
+    val jacksonDataFrame = ReferenceRecordJacksonIO.readDataFrame(Config.JACKSON_FILE_NAME, Config.TARGET_FOLDER)
+    println(jacksonDataFrame.generateCode(markerName = "ReferenceRecord"))
 }
--- src/main/kotlin/ReferenceRecordJacksonIO.kt
+++ src/main/kotlin/ReferenceRecordJacksonIO.kt
@@ -3,6 +3,8 @@ package org.example
 import com.fasterxml.jackson.dataformat.csv.CsvMapper
 import com.fasterxml.jackson.dataformat.csv.CsvParser
 import com.fasterxml.jackson.dataformat.csv.CsvSchema
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.api.toDataFrame
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileInputStream
@@ -11,6 +13,11 @@ import java.nio.charset.StandardCharsets
 
 class ReferenceRecordJacksonIO {
     companion object {
+        fun readDataFrame(fileName: String, targetFolder: String): DataFrame<ReferenceRecord> {
+            val list = read(fileName, targetFolder)
+            return list.toDataFrame()
+        }
+
         fun read(fileName: String, folder: String): List<ReferenceRecord> {
             val parentFolder = CpvFolder.get(fileName, folder)
             FileInputStream(File(parentFolder, fileName))
